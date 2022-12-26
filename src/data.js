1011c795a33dbc591c2bc649ac92aa967a9e5682
Rohit <rohitexture099@gmail.com>
2022-12-27 01:23:07 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "Vyom Voyage",
+    subtitle: "MERN Stack",
+    description:
+    "This website use by the Technical Team of TCET which were participating on global competition for Nano Satellite Competition.The site contains information about the CubeSat, Team Members and Research work.",
+    image: "./project-1.gif",
+    link: "https://roh099.github.io/Satellite-Website/",
+  },
+  {
+    title: "Laptop Price Predictor using ML",
+    subtitle: "Python and Machine Learning",
+    description:
+      "A simple Machine learning web app deployed on Heroku.If any user wants to buy a laptop then our Application should be compatible to provide a tentative price of laptop according to the user configurations.",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DRESSCODE The Fashion Portal Website",
+    subtitle: " HTML,CCS,JS and BOOTSTRAP",
+    description:"Website made to provide all the information about Fashion.It also has a different category such as Kids, Men and Women as well as login and contact form.Created site Layout and user interface. ",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Coming soon",
+    subtitle: "Tech Stack",
+    description:" Worked for the NGO to design their brouchers, logo, blogs and post of the NGO as well as handling their social media account.Responsible for daily production of small but innovative design firm.",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    image: "https://i.ibb.co/Nrgfc7y/sample.png",    
+  },
+  {
+    image: "https://i.ibb.co/ZhZ5gyz/sample2.png" 
+  },
+];
+
+export const skills = [
+  "Java and C++",
+  "Data Structure & Algorithm",
+  "HTML, CSS and Bootstrap",
+  "ReactJS",
+  "Operating System",
+  "Canva and Figma",
+  "Python",
+  "Machine Learning"
+];
